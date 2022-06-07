e7f68c75023f2499cc812bd35ebca2bddebd745d
solvotvptogramming <uvv0106@gmail.com>
2022-06-07 14:20:06 +0800

    first

---
+++ Simple_APP/Entrance.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_APP
+{
+    /// <summary>
+    /// Логика взаимодействия для Entrance.xaml
+    /// </summary>
+    public partial class Entrance : Page
+    {
+        public Entrance()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Menu());
+        }
+    }
+}
